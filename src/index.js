b19c6c334984b6cf93a8e4507ec71c678d990840
Caio Rodrigues <rfcaio90@gmail.com>
2019-01-14 13:51:36 -0300

    asset management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import _ from 'lodash'
 
+import './style.css'
+import data from './data.xml'
+import image from './image.png'
+
 function component () {
+  console.log(data)
   let element = document.createElement('div')
-  element.innerHTML = _.join(['You', 'will', 'never', 'win!'], ' ')
+  let h1 = document.createElement('h1')
+  h1.classList.add('quotes')
+  h1.textContent = _.join(['You', 'will', 'never', 'win!'], ' ')
+  let img = new Image()
+  img.width = '300'
+  img.src = image
+  element.appendChild(img)
+  element.appendChild(h1)
   return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,32 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|jpg|png|svg|)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(eot|otf|ttf|woff|woff2)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
+  },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
